32520e8b5fdd9994b67775f1e87af89853509f32
Łukasz Pior <piorek82@gmail.com>
2020-07-21 13:21:14 +0200

    prettier webpack.config.js

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -3,20 +3,20 @@ const path = require('path');
 var rules = [
   {
     test: /\.css$/i,
-    use: [{loader: "style-loader"}, {loader: "css-loader"}],
-    exclude: [/katex\.css$/i]
+    use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+    exclude: [/katex\.css$/i],
   },
   {
     test: /katex\.css$/i,
-    use: [{loader: "css-loader"}],
+    use: [{ loader: 'css-loader' }],
   },
   {
     test: /\.scss$/,
-    use: [{loader: "style-loader"}, {loader: "css-loader"}, {loader: "sass-loader"}]
+    use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
   },
   {
     test: /\.(jpg|png|gif)$/,
-    use: {loader: "file-loader"}
+    use: { loader: 'file-loader' },
   },
   {
     test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
@@ -26,7 +26,7 @@ var rules = [
         limit: 10000,
         mimetype: 'application/font-woff',
         name: './css/fonts/[name].[ext]',
-      }
+      },
     },
   },
   {
@@ -36,34 +36,34 @@ var rules = [
       options: {
         limit: 10000,
         mimetype: 'application/octet-stream',
-        name: "./css/fonts/[name].[ext]",
-      }
-    }
+        name: './css/fonts/[name].[ext]',
+      },
+    },
   },
   {
     test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
     use: {
-      loader: "file-loader"
-    }
+      loader: 'file-loader',
+    },
   },
   {
     test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
     use: {
-      loader: "url-loader",
+      loader: 'url-loader',
       options: {
         limit: 10000,
-        mimetype: "image/svg+xml",
+        mimetype: 'image/svg+xml',
         name: './css/fonts/[name].[ext]',
-      }
-    }
-  }
+      },
+    },
+  },
 ];
 
 module.exports = [
   // Notebook extension
   {
-    "entry": './lib/extension.js',
-    "output": {
+    entry: './lib/extension.js',
+    output: {
       filename: 'extension.js',
       path: path.resolve(__dirname, '../beakerx_widgets/beakerx_widgets/static'),
       libraryTarget: 'amd',
@@ -83,52 +83,52 @@ module.exports = [
       'notebook/js/codecell',
     ],
     module: {
-      rules: rules
+      rules: rules,
     },
-    mode: 'production'
+    mode: 'production',
   },
   {
-    "entry": './lib/tree-extension.js',
-    "output": {
+    entry: './lib/tree-extension.js',
+    output: {
       filename: 'tree-extension.js',
       path: path.resolve(__dirname, '../beakerx_widgets/beakerx_widgets/static'),
       libraryTarget: 'amd',
     },
     devtool: 'inline-source-map',
     module: {
-      rules: rules
+      rules: rules,
     },
     externals: ['@jupyter-widgets/base', '@jupyter-widgets/controls', 'jquery'],
-    mode: 'production'
+    mode: 'production',
   },
   // beakerx_widgets bundle for the classic notebook
   {
-    "entry": './lib/index-classic.js',
-    "output": {
+    entry: './lib/index-classic.js',
+    output: {
       filename: 'index.js',
       path: path.resolve(__dirname, '../beakerx_widgets/beakerx_widgets/static'),
       libraryTarget: 'amd',
     },
     devtool: 'inline-source-map',
     module: {
-      rules: rules
+      rules: rules,
     },
     externals: ['@jupyter-widgets/base', '@jupyter-widgets/controls', 'jquery'],
-    mode: 'production'
+    mode: 'production',
   },
   // beakerx_widgets bundle for unpkg.
   {
-    "entry": './lib/index-embed.js',
-    "output": {
+    entry: './lib/index-embed.js',
+    output: {
       filename: 'index.js',
       path: path.resolve(__dirname, './dist/'),
       libraryTarget: 'amd',
     },
     devtool: 'inline-source-map',
     module: {
-      rules: rules
+      rules: rules,
     },
     externals: ['@jupyter-widgets/base', '@jupyter-widgets/controls'],
-    mode: 'production'
-  }
+    mode: 'production',
+  },
 ];
